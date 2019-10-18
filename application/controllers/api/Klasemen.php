5ad40588cbfc16eb04e1b0cd55cef68fa0fca2e9
bayufajariyanto <bayuf08@gmail.com>
2019-10-18 22:02:09 +0700

    RESTful API

--- application/controllers/api/Klasemen.php
+++ application/controllers/api/Klasemen.php
@@ -72,4 +72,59 @@ class Klasemen extends REST_Controller
             }
         }
     }
+
+    public function index_post()
+    {
+        $data = [
+            'name' => $this->post('name'),
+            'p' => $this->post('p'),
+            'w' => $this->post('w'),
+            'd' => $this->post('d'),
+            'l' => $this->post('l'),
+            'f' => $this->post('f'),
+            'a' => $this->post('a'),
+            'gd' => $this->post('gd'),
+            'pts' => $this->post('pts')
+        ];
+
+        if ($this->klasemen->createKlasemen($data) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new klasemen has been created.'
+            ], REST_Controller::HTTP_CREATED);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'failed to create new data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function index_put()
+    {
+        $id = $this->put('id');
+        $data = [
+            'name' => $this->put('name'),
+            'p' => $this->put('p'),
+            'w' => $this->put('w'),
+            'd' => $this->put('d'),
+            'l' => $this->put('l'),
+            'f' => $this->put('f'),
+            'a' => $this->put('a'),
+            'gd' => $this->put('gd'),
+            'pts' => $this->put('pts')
+        ];
+
+        if ($this->klasemen->updateKlasemen($data, $id) > 0) {
+            $this->response([
+                'status' => true,
+                'message' => 'new klasemen has been updated.'
+            ], REST_Controller::HTTP_NO_CONTENT);
+        } else {
+            $this->response([
+                'status' => false,
+                'message' => 'failed to update data!'
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
 }
